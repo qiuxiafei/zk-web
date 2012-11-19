8d91b3f867b1302b16ce15b0b09160a84c52980c
dennis zhuang <killme2008@gmail.com>
2012-11-19 12:02:34 +0800

    Impls default-node feature and move config file directory

---
+++ conf/zk-web-conf.clj
@@ -0,0 +1,5 @@
+{
+ :server-port 8080
+ :users {"admin" "hello"}
+ :default-node ""
+}
--- src/zk_web/conf.clj
+++ src/zk_web/conf.clj
@@ -2,20 +2,23 @@
   (:require [clojure.java.io :as io])
   (:import [java.io File PushbackReader]))
 
-(defn valid-conf-file?
+(defn- valid-conf-file?
   "Check if a file exists and is a normal file"
   [path]
   (let [file (File. path)]
     (and (.exists file)
          (.isFile file))))
 
+(defn- load-conf-file [path]
+  (when (valid-conf-file? path)
+    (read-string (slurp path :encoding "utf-8"))))
+
 (defn load-conf []
-  "load the config from ~/.zk-web-conf.clj"
-  (let [file (str (get (System/getenv) "HOME") File/separator ".zk-web-conf.clj")]
-    (if (valid-conf-file? file)
-      (with-open [r (io/reader file)]
-        (read (PushbackReader. r)))
-      {
-       :server-port 8080
-       :users {"admin" "hello"}
-       })))
+  "load the config from ~/.zk-web-conf.clj or conf/zk-web-conf.clj"
+  (let [home-conf (str (get (System/getenv) "HOME") File/separator ".zk-web-conf.clj")
+        pwd-conf "conf/zk-web-conf.clj"]
+    (or (load-conf-file home-conf) (load-conf-file pwd-conf)
+        {
+         :server-port 8080
+         :users {"admin" "hello"}
+         })))
